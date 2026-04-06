2659849cc46e05145e9fe98a7151d7d52e8888a5
romanoleksivpp2022 <roman.oleksiv.pp.2022@lpnu.ua>
2026-04-06 23:58:05 +0300

    Update main.tf

--- task1/main.tf
+++ task1/main.tf
@@ -1,1 +1,25 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
 
+  backend "s3" {
+    endpoints = {
+      s3 = "https://fra1.digitaloceanspaces.com"
+    }
+    bucket                      = "oleksiv-tfstate"
+    key                         = "terraform.tfstate"
+    region                      = "us-east-1"
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    force_path_style            = true
+  }
+}
+
+provider "digitalocean" {
+  token = var.do_token
+}
