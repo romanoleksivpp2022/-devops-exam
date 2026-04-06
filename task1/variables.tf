c3b45b6803fb19b7eee764503cdfbbec7cbc5832
romanoleksivpp2022 <roman.oleksiv.pp.2022@lpnu.ua>
2026-04-07 00:00:40 +0300

    Create variables.tf

---
+++ task1/variables.tf
@@ -0,0 +1,17 @@
+variable "do_token" {
+  description = "DigitalOcean API Token"
+  type        = string
+  sensitive   = true
+}
+
+variable "region" {
+  description = "DigitalOcean region"
+  type        = string
+  default     = "fra1"
+}
+
+variable "prefix" {
+  description = "Resource name prefix"
+  type        = string
+  default     = "oleksiv"
+}
